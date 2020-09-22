f63a24b64417063c1a0c60daa05e10481c1ac0eb
Mike Champagne <geauxdrum@gmail.com>
2020-09-22 14:03:00 -0500

    updates ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,22 @@
 // sets up PM2 to create a running build on an amazon EC2 server
 
 module.exports = {
-  apps: [{
-    name: 'champagne_products',
-    script: './server/index.js'
-  }],
+  apps: [
+    {
+      name: "champagne_products",
+      script: "./server/index.js",
+    },
+  ],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-3-15-234-135.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/front-end-capstone.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:ATX-50-Team-Best-Buy/champagne_products.git',
-      path: '/home/ubuntu/champagne_products',
-      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
-    }
-  }
-}
+      user: "ubuntu",
+      host: "ec2-18-218-93-73.us-east-2.compute.amazonaws.com",
+      key: "~/.ssh/front-end-capstone.pem",
+      ref: "origin/master",
+      repo: "git@github.com:ATX-50-Team-Best-Buy/champagne_products.git",
+      path: "/home/ubuntu/champagne_products",
+      "post-deploy":
+        "npm install && npm run build && pm2 startOrRestart ecosystem.config.js",
+    },
+  },
+};
